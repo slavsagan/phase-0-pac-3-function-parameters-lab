b727731bf5882f7ac34ca14d8ea2830bcc028a84
Slav <itiksolution@gmail.com>
2022-07-20 12:54:41 +1000

    asignment complete

---
+++ index.js
@@ -0,0 +1,21 @@
+function introduction(name) {
+  return `Hi, my name is ${name}.`
+}
+console.log (introduction("Aki"))
+console.log (introduction("Samip"))
+//-----------------------------------//
+function introductionWithLanguage(name, language) {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+console.log (introductionWithLanguage("Aki", "Ember.js"))
+console.log (introductionWithLanguage("Samip", "React"))
+//-----------------------------------//
+function introductionWithLanguageOptional(name = "Gracie", language = "JavaScript") {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+console.log (introductionWithLanguageOptional())
+//-----------------------------------//
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+console.log (introductionWithLanguageOptional("Gracie", "Python"))
